c87e5acdab0228a2d74fe80b3196e0db5b1d8531
Asad Ali <asadrajput2@gmail.com>
2020-11-11 12:21:36 +0530

    added routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 import Login from './login';
 import Signup from './signup';
+import ProductsList from './productsList/products_list'
 
 
 export default function App(props) {
     return (
-        <div>
+        <BrowserRouter>
+            <div>
 
-            <nav className='nav'>
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/login/">Login</Link>
-                <Link className="nav-link" to="/signup/">Signup</Link>
-            </nav>
+                <nav className='nav'>
+                    <Link className="nav-link" to="/">Home</Link>
+                    <Link className="nav-link" to="/login/">Login</Link>
+                    <Link className="nav-link" to="/signup/">Signup</Link>
+                </nav>
 
-            <main>
-                <Switch>
-                    <Route exact path={"/login/"} component={Login} />
-                    <Route exact path={"/signup/"} component={Signup} />
-                    <Route path={"/"} render={() => <h1>Home Again</h1>} />
-                </Switch>
-            </main>
-        </div>
+                <main>
+                    <Switch>
+                        <Route exact path={"/login/"} component={Login} />
+                        <Route exact path={"/signup/"} component={Signup} />
+                        <Route exact path={"/products/"} component={ProductsList} />
+                        <Route path={"/"} render={() => <h1>Home Again</h1>} />
+                    </Switch>
+                </main>
+            </div>
+        </BrowserRouter>
     );
 };
